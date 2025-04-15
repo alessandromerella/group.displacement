8eb9230d0501a8f7b2a9216c864b2ee8fed6e87a
alessandromerella <133819402+alessandromerella@users.noreply.github.com>
2025-04-15 06:52:16 +0200

    app.py

--- app.py
+++ app.py
@@ -1048,7 +1048,7 @@ st.markdown("---")
 st.markdown(
    f"""
    <div style='text-align: center; font-family: Inter, sans-serif; color: #5E5E5E; font-size: 0.8rem;'>
-       <p>Group Displacement Analyzer | v0.4.5 developed by Alessandro Merella; Original excel concept and formulas by Andrea Conte<br>
+       <p>Group Displacement Analyzer | v0.4.5 developed by Alessandro Merella | Original excel concept and formulas by Andrea Conte<br>
        Sessione: {st.session_state['username']} | Ultimo accesso: {datetime.fromtimestamp(st.session_state['login_time']).strftime('%d/%m/%Y %H:%M')}
        </p>
    </div>
